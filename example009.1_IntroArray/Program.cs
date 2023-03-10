0b283b6551a2c89bdf98429d1db6db3a81723b37
Алексей <ezekiil@yandex.ru>
2023-03-11 00:12:20 +0300

    add seminar 2 Array

---
+++ example009.1_IntroArray/Program.cs
@@ -0,0 +1,19 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2; 
+    if (arg3 > result) result = arg3;
+    return result;
+}
+//               0   1   2   3   4   5   6   7   8   9 
+int [] array = {11, 21, 31, 41, 51, 61, 17, 18, 19, 10};
+array[0] = 12; // изменение значения первого элемента массива
+System.Console.WriteLine($"Четвертый элемент массива: {array[4]}"); // вывод значения четвертого элемента массива
+
+int max = Max(array[0], array[1], array[2]);
+for (int i = 3; i < array.Length; i++)
+{
+    if (array[i] > max) max = array[i];
+}
+System.Console.WriteLine($"Максимальное число: {max}");
+
---
+++ example009.3_IntroArray/Program.cs
@@ -0,0 +1,42 @@
+﻿void FillArray(int[] collection) // заполнение массива случайными числами
+{
+    int length = collection.Length; // длина массива
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int[] col) // вывод массива
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        System.Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int IndexOf(int[] collection, int find) // поиск индекса искомого элемента
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine();
+int pos = IndexOf(array, 4);
+System.Console.WriteLine($"Индекс первого числа четыре: {pos}");
